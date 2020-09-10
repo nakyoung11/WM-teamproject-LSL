996a292e956e69bde62ada98a9057ed07af4a11c
Lee <dlehdud1653@kakao.com>
2020-09-10 10:46:23 +0900

    style.css 오류 수정

--- WM/src/com/lsl/wm/user_writer/MypageConfSer.java
+++ WM/src/com/lsl/wm/user_writer/MypageConfSer.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/mypage_conf")
+@WebServlet("/writer/mypage_conf")
 public class MypageConfSer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
