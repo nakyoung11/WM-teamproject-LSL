154d544275f28658063563bd50fdbd6245273a4e
Lee <dlehdud1653@kakao.com>
2020-09-16 19:30:26 +0900

    ~님 안녕하세요 구현

--- WM/src/com/lsl/wm/MyUtils.java
+++ WM/src/com/lsl/wm/MyUtils.java
@@ -8,6 +8,6 @@ import com.lsl.wm.vo.UserVO;
 public class MyUtils {
 	public static UserVO getLoginUser(HttpServletRequest request) {
 		HttpSession hs = request.getSession();
-		return (UserVO)hs.getAttribute("loginUser");
+		return (UserVO)hs.getAttribute("loginUser"); // 로그인 서블릿에서 doGet메소드에서 호출하면 doPost에서 set된 세션을 가져옴
 	}
 }
