4d35617d6974189a718e5038dc26663fec3ee4c7
LEE DO YOUNG <dlehdud1653@kakao.com>
2020-10-11 17:41:55 +0900

    공지사항 리스트 출력

---
+++ WM/src/com/lsl/wm/vo/NoticeVO.java
@@ -0,0 +1,55 @@
+package com.lsl.wm.vo;
+
+public class NoticeVO {
+	private int i_notice;
+	private String title;
+	private String ctnt;
+	private int i_user;
+	private String r_dt;
+	private String m_dt;
+	private String nm;
+	
+	public int getI_notice() {
+		return i_notice;
+	}
+	public void setI_notice(int i_notice) {
+		this.i_notice = i_notice;
+	}
+	public String getNm() {
+		return nm;
+	}
+	public void setNm(String nm) {
+		this.nm = nm;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCtnt() {
+		return ctnt;
+	}
+	public void setCtnt(String ctnt) {
+		this.ctnt = ctnt;
+	}
+	public int getI_user() {
+		return i_user;
+	}
+	public void setI_user(int i_user) {
+		this.i_user = i_user;
+	}
+	public String getR_dt() {
+		return r_dt;
+	}
+	public void setR_dt(String r_dt) {
+		this.r_dt = r_dt;
+	}
+	public String getM_dt() {
+		return m_dt;
+	}
+	public void setM_dt(String m_dt) {
+		this.m_dt = m_dt;
+	}
+	
+}
