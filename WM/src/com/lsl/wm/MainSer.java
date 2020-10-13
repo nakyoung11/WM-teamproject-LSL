2eb926478cacebe27049c12763a4bfdb8aeff92f
DoYoung LEE <dlehdud1653@kakao.com>
2020-10-13 16:01:39 +0900

    메인에 작품올리기

--- WM/src/com/lsl/wm/MainSer.java
+++ WM/src/com/lsl/wm/MainSer.java
@@ -3,6 +3,7 @@ package com.lsl.wm;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
@@ -11,7 +12,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.lsl.wm.db.ShowDAO;
 import com.lsl.wm.db.UserDAO;
+import com.lsl.wm.vo.ShowVO;
 
 
 @WebServlet("/main")
@@ -23,7 +26,16 @@ public class MainSer extends HttpServlet {
 		
 		String str = String.format("%03d", UserDAO.selUserCnt()) ;
 		
+		ShowVO param = new ShowVO();
+		param.setRow(0);
+		param.setSearchText("%%");
+		List<ShowVO> list = ShowDAO.selShowList(param);
+		request.setAttribute("list", list);
+		
+		String savePath = "/resource/show/images/posters/";
+		
 		request.setAttribute("userCnt", str);
+		request.setAttribute("posterPath", savePath);
 		String jsp = "/WEB-INF/view/main.jsp";
 		request.getRequestDispatcher(jsp).forward(request, response);
 	}
