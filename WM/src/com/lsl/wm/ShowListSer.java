75f1562b10e57149cfa07583dbb78287ad202d08
nakyoung11 <nakihome@naver.com>
2020-10-13 14:44:19 +0900

    showListSer 고침

--- WM/src/com/lsl/wm/ShowListSer.java
+++ WM/src/com/lsl/wm/ShowListSer.java
@@ -27,12 +27,7 @@ public class ShowListSer extends HttpServlet {
 	            return;
 	         }
 	       
-	      String searchText = request.getParameter("searchText");
-	      searchText = (searchText == null ? "" : searchText);
-	      String strPage = request.getParameter("page");
-	      if(strPage == null) {
-	         strPage = "0";
-	      }
+	 
     
 		String searchText = request.getParameter("searchText");
 		searchText = (searchText == null ? "" : searchText);
